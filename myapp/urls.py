7e04969cd41776f035082d082d7d55d1889fbac2
sarveshkrg <46168717+sarveshkrg@users.noreply.github.com>
2018-12-29 19:12:30 +0530

    .

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path(r'', views.index, name='index'),
     #path(r'signup/', views.signup, name='signup'),
     path(r'signup/', views.user_create_view, name='user_create_view'),
+    path(r'login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
+    path(r'logout/', auth_views.LogoutView.as_view(template_name='myapp/logout.html'), name='logout'),
     path(r'profile/', views.profile, name='profile'),
 ]
